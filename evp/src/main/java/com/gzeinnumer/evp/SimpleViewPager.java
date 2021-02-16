5e7fa7a108bf407f6f131a0758e7560e19cfacb8
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-02-17 00:35:09 +0700

    Initial commit

---
+++ evp/src/main/java/com/gzeinnumer/evp/SimpleViewPager.java
@@ -0,0 +1,102 @@
+package com.gzeinnumer.evp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.material.tabs.TabLayout;
+import com.gzeinnumer.evp.adapter.ViewPagerAdapter;
+import com.gzeinnumer.evp.ui.ViewPagerNonSwipable;
+
+import java.util.ArrayList;
+
+public class SimpleViewPager extends LinearLayout {
+    Context context;
+    AttributeSet attrs;
+
+    public SimpleViewPager(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        this.attrs = attrs;
+
+        initView();
+    }
+
+    TabLayout tabLayout = null;
+    ViewPagerNonSwipable viewPager;
+    TabLayout dotsTab = null;
+
+    ViewPagerAdapter viewPagerAdapter;
+
+    boolean enableClickDot = false;
+    boolean enableSwipe = true;
+
+    private void initView() {
+        inflate(context, R.layout.custom_viewpager, this);
+
+        viewPager = findViewById(R.id.view_pager);
+    }
+
+    public SimpleViewPager setAdapter(ViewPagerAdapter adapter){
+        viewPagerAdapter = adapter;
+        return this;
+    }
+
+    public void build(){
+        viewPager.setSwipeable(enableSwipe);
+        viewPager.setAdapter(viewPagerAdapter);
+        if (tabLayout!=null){
+            if (viewPagerAdapter.getTitles().size()==0){
+                tabLayout.setVisibility(GONE);
+            }
+            tabLayout.setupWithViewPager(viewPager);
+
+            if (viewPagerAdapter.getFragments().size()>0){
+                for (int i=0; i<viewPagerAdapter.getIcons().size(); i++){
+                    tabLayout.getTabAt(i).setIcon(viewPagerAdapter.getIcons().get(i));
+                }
+            }
+        } else if (dotsTab!=null){
+            viewPagerAdapter.setTitles(new ArrayList<>());
+            viewPagerAdapter.setIcons(new ArrayList<>());
+
+            dotsTab.setupWithViewPager(viewPager);
+
+            if (!enableClickDot){
+                LinearLayout tabStrip = ((LinearLayout) dotsTab.getChildAt(0));
+                for (int i = 0; i < tabStrip.getChildCount(); i++) {
+                    tabStrip.getChildAt(i).setOnTouchListener(new OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            return true;
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    public SimpleViewPager enableSwipe(boolean enableSwipe) {
+        this.enableSwipe = enableSwipe;
+        return this;
+    }
+
+    public SimpleViewPager setTabLayout(TabLayout tabLayout) {
+        this.tabLayout = tabLayout;
+        return this;
+    }
+
+    public SimpleViewPager setDotsTab(TabLayout dotsTab, boolean enableClickDot) {
+        this.dotsTab = dotsTab;
+        this.enableClickDot = enableClickDot;
+        return this;
+    }
+
+    public void setSelectedPosition(int position) {
+        viewPager.setCurrentItem(position);
+    }
+}
